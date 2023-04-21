73d1eca44deaf48efbb72d0c435695e4ae948f88
Tondo <tondinialessio@gmail.com>
2023-04-21 16:24:40 +0200

    Creazione GameInfo

--- Assets/GameInfo.cs
+++ Assets/GameInfo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameInfo : MonoBehaviour
 {
+    public string nome;
+    public Rarity rarity;
+    public bool originale;
     // Start is called before the first frame update
     void Start()
     {
